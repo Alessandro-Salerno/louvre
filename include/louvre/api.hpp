9c78c62569b5d6330178c40f6413f9ffa2dcee0d
Alessandro-Salerno <oxygen05@outlook.it>
2025-03-23 16:21:29 +0100

    ported parser

---
+++ include/louvre/api.hpp
@@ -0,0 +1,256 @@
+/* Copyright 2025 Alessandro Salerno
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include <string_view>
+#include <unordered_map>
+
+namespace louvre {
+enum class ParserAction { End, AddChild, AddChildAndBranch, Ignore };
+
+enum class StandardNodeType {
+    Root,
+    Left,
+    Center,
+    Right,
+    Justify,
+    Paragraph,
+    Numebrs,
+    Bullets,
+    Item,
+    Text,
+    LineBreak,
+    Null,
+    Group,
+    Custom,
+};
+
+class SourceLocation {
+    private:
+    const std::size_t mLine;
+    const std::size_t mColumn;
+
+    public:
+    SourceLocation(std::size_t line, std::size_t column)
+        : mLine(line), mColumn(column) {};
+
+    inline const std::size_t line() const {
+        return this->mLine;
+    }
+
+    inline const std::size_t column() const {
+        return this->mColumn;
+    }
+};
+
+class Tag {
+    private:
+    const std::wstring_view        mName;
+    const SourceLocation           mLocation;
+    std::vector<std::wstring_view> mArguments;
+
+    public:
+    Tag(std::wstring_view name, SourceLocation location)
+        : mName(name), mLocation(location) {};
+
+    inline const std::wstring_view name() const {
+        return this->mName;
+    }
+
+    inline const SourceLocation location() const {
+        return this->mLocation;
+    }
+
+    inline const std::vector<std::wstring_view> arguments() const {
+        return this->mArguments;
+    }
+
+    inline void add_argument(std::wstring_view argument) {
+        this->mArguments.push_back(argument);
+    }
+};
+
+class Node : std::enable_shared_from_this<Node> {
+    private:
+    const std::variant<StandardNodeType, std::wstring_view> mType;
+    std::optional<std::wstring_view>                        mText;
+    std::optional<std::shared_ptr<Tag>>                     mTag;
+    std::optional<std::shared_ptr<Node>>                    mParent;
+    std::vector<std::shared_ptr<Node>>                      mChildren;
+
+    public:
+    Node() : Node(StandardNodeType::Root) {};
+    Node(StandardNodeType type) : mType(type) {};
+    Node(std::wstring_view type) : mType(type) {};
+    Node(Node &&other) noexcept;
+
+    static inline Node text(std::wstring_view text) {
+        Node n(StandardNodeType::Text);
+        n.mText = text;
+        return n; // ret val optimization helps here
+    }
+
+    inline const std::variant<StandardNodeType, std::wstring_view>
+    type() const {
+        return this->mType;
+    }
+
+    inline const std::optional<std::wstring_view> text() const {
+        return this->mText;
+    }
+
+    inline const std::optional<std::shared_ptr<Tag>> tag() const {
+        return this->mTag;
+    }
+
+    inline const std::optional<std::shared_ptr<Node>> parent() const {
+        return this->mParent;
+    }
+
+    inline const std::vector<std::shared_ptr<Node>> children() const {
+        return this->mChildren;
+    }
+
+    inline void add_child(std::shared_ptr<Node> child) {
+        this->add_dangling_child(child);
+        child->set_parent(this->shared_from_this());
+    }
+
+    inline void add_dangling_child(std::shared_ptr<Node> child) {
+        this->mChildren.push_back(child);
+    }
+
+    inline void set_parent(std::shared_ptr<Node> parent) {
+        this->mParent = parent;
+    }
+
+    inline void set_tag(std::shared_ptr<Tag> tag) {
+        this->mTag = tag;
+    }
+};
+
+class SyntaxError {
+    private:
+    const std::wstring_view mMessage;
+    const SourceLocation    mLocation;
+
+    public:
+    SyntaxError(std::wstring_view message, SourceLocation location)
+        : mMessage(message), mLocation(location) {};
+
+    inline const std::wstring_view message() const {
+        return this->mMessage;
+    }
+
+    inline const SourceLocation location() const {
+        return this->mLocation;
+    }
+};
+
+class TagError {
+    private:
+    const std::wstring_view    mMessage;
+    const std::shared_ptr<Tag> mTag;
+
+    public:
+    TagError(std::wstring_view message, std::shared_ptr<Tag> tag)
+        : mMessage(message), mTag(tag) {};
+
+    inline const std::wstring_view message() const {
+        return this->mMessage;
+    }
+
+    inline const std::shared_ptr<Tag> tag() const {
+        return this->mTag;
+    }
+};
+
+class NodeError {
+    private:
+    const std::wstring_view     mMessage;
+    const std::shared_ptr<Node> mNode;
+
+    public:
+    NodeError(std::wstring_view message, std::shared_ptr<Node> node)
+        : mMessage(message), mNode(node) {};
+
+    inline const std::wstring_view message() const {
+        return this->mMessage;
+    }
+
+    inline const std::shared_ptr<Node> node() const {
+        return this->mNode;
+    }
+};
+
+class Parser {
+    private:
+    const std::wstring_view mSource;
+    std::unordered_map<
+        std::wstring_view,
+        std::function<std::pair<ParserAction, Node>(std::shared_ptr<Tag>)>>
+                mTagBindings;
+    std::size_t mOffset;
+    std::size_t mLine;
+    std::size_t mColumn;
+
+    public:
+    Parser(std::wstring_view source) : mSource(source) {};
+
+    inline void add_tag_binding(
+        std::wstring_view tag,
+        std::function<std::pair<ParserAction, Node>(std::shared_ptr<Tag>)>
+            binding) {
+        this->mTagBindings[tag] = binding;
+    }
+
+    std::variant<std::shared_ptr<Node>, SyntaxError, TagError, NodeError>
+    parse();
+
+    private:
+    static inline bool          is_tag_char(wchar_t c);
+    static inline std::wstring  trim(std::wstring &s);
+    inline const SourceLocation location() const;
+    inline bool                 can_advance(std::size_t amount = 1) const;
+    inline void                 advance(std::size_t amount = 1);
+    inline void                 backtracK(std::size_t amount = 1);
+    inline const std::optional<wchar_t> peek(std::size_t ahead = 0) const;
+    inline wchar_t                      quick_peek(std::size_t ahead = 0) const;
+    inline void                         advance_line();
+    inline wchar_t                      consume();
+    void                                skip_whitespace();
+    const std::variant<wchar_t, SyntaxError>
+                 consume_if(const std::wstring_view &allowed);
+    std::wstring collect_sequence();
+    const std::variant<std::shared_ptr<Tag>, SyntaxError> collect_tag();
+    const std::variant<std::pair<ParserAction, std::shared_ptr<Node>>, TagError>
+    tag_to_node(std::shared_ptr<Tag> tag);
+    const std::optional<
+        std::variant<std::pair<ParserAction, std::shared_ptr<Node>>,
+                     SyntaxError,
+                     TagError>>
+    collect_block();
+};
+
+} // namespace louvre
---
+++ src/parser.cpp
@@ -0,0 +1,273 @@
+/* Copyright 2025 Alessandro Salerno
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include <algorithm>
+#include <cwctype>
+#include <louvre/api.hpp>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <variant>
+
+namespace louvre {
+std::variant<std::shared_ptr<Node>, SyntaxError, TagError, NodeError>
+Parser::parse() {
+    auto root = std::make_shared<Node>();
+
+    while (this->can_advance()) {
+        const std::optional<
+            std::variant<std::pair<ParserAction, std::shared_ptr<Node>>,
+                         SyntaxError,
+                         TagError>>
+            block_opt = this->collect_block();
+
+        if (!block_opt) {
+            break;
+        }
+
+        const auto block_res = block_opt.value();
+
+        if (std::holds_alternative<SyntaxError>(block_res)) {
+            return std::get<SyntaxError>(block_res);
+        }
+
+        if (std::holds_alternative<TagError>(block_res)) {
+            return std::get<TagError>(block_res);
+        }
+
+        const auto [action, node] =
+            std::get<std::pair<ParserAction, std::shared_ptr<Node>>>(block_res);
+
+        switch (action) {
+        case ParserAction::AddChild:
+            root->add_child(node);
+            break;
+
+        case ParserAction::AddChildAndBranch:
+            root->add_child(node);
+            root = node;
+            break;
+
+        case ParserAction::End:
+            if (!root->parent()) {
+                return NodeError(L"Unexpected branch return at root level",
+                                 node);
+            }
+
+            root = root->parent().value();
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    return root;
+}
+
+inline std::wstring Parser::trim(std::wstring &s) {
+    s.erase(std::find_if(s.rbegin(),
+                         s.rend(),
+                         [](unsigned char ch) { return !std::iswspace(ch); })
+                .base(),
+            s.end());
+
+    return s;
+}
+
+inline bool Parser::is_tag_char(wchar_t c) {
+    return std::iswalnum(c) || '_' == c;
+}
+
+inline const SourceLocation Parser::location() const {
+    return SourceLocation(this->mLine, this->mColumn);
+}
+
+inline bool Parser::can_advance(std::size_t amount) const {
+    return this->mOffset + amount < this->mSource.length();
+}
+
+inline void Parser::advance(std::size_t amount) {
+    this->mOffset += amount;
+    this->mColumn += amount;
+}
+
+inline void Parser::backtracK(std::size_t amount) {
+    this->advance(-amount);
+}
+
+inline const std::optional<wchar_t> Parser::peek(std::size_t ahead) const {
+    if (this->can_advance(ahead)) {
+        return this->mSource[this->mOffset + ahead];
+    }
+
+    return std::nullopt;
+}
+
+inline wchar_t Parser::quick_peek(std::size_t ahead) const {
+    return this->peek(ahead).value_or(L'\0');
+}
+
+inline void Parser::advance_line() {
+    this->mLine++;
+    this->mColumn = 0;
+}
+
+inline wchar_t Parser::consume() {
+    wchar_t c = this->quick_peek();
+    this->advance();
+    return c;
+}
+
+void Parser::skip_whitespace() {
+}
+
+const std::variant<wchar_t, SyntaxError>
+Parser::consume_if(const std::wstring_view &allowed) {
+    if (!this->peek().has_value()) {
+        return SyntaxError(L"Unexpected EOF", this->location());
+    }
+
+    if (std::wstring_view::npos == allowed.find(this->quick_peek())) {
+        return SyntaxError(L"Unexpected token", this->location());
+    }
+
+    return this->consume();
+}
+
+std::wstring Parser::collect_sequence() {
+    std::wstring buf;
+
+    while (this->can_advance() && Parser::is_tag_char(this->quick_peek())) {
+        buf.push_back(this->consume());
+    }
+
+    return buf;
+}
+
+const std::variant<std::shared_ptr<Tag>, SyntaxError> Parser::collect_tag() {
+    std::wstring tag_name = this->collect_sequence();
+    auto         tag      = std::make_shared<Tag>(tag_name, this->location());
+
+    if (std::holds_alternative<SyntaxError>(this->consume_if(L"("))) {
+        return tag;
+    }
+
+    while (true) {
+        this->skip_whitespace();
+        std::wstring arg = this->collect_sequence();
+
+        if (!arg.empty()) {
+            tag->add_argument(arg);
+        }
+
+        std::variant<wchar_t, SyntaxError> next = this->consume_if(L",)");
+
+        if (std::holds_alternative<SyntaxError>(next)) {
+            return std::get<SyntaxError>(next);
+        }
+
+        if (L')' == std::get<wchar_t>(next)) {
+            break;
+        }
+    }
+
+    return tag;
+}
+
+const std::variant<std::pair<ParserAction, std::shared_ptr<Node>>, TagError>
+Parser::tag_to_node(std::shared_ptr<Tag> tag) {
+    if (this->mTagBindings.contains(tag->name())) {
+        auto handler        = this->mTagBindings.at(tag->name());
+        auto [action, node] = handler(tag);
+        node.set_tag(tag);
+        return std::make_pair(action, std::make_shared<Node>(std::move(node)));
+    }
+
+    return TagError(L"Unknown tag", tag);
+}
+
+const std::optional<std::variant<std::pair<ParserAction, std::shared_ptr<Node>>,
+                                 SyntaxError,
+                                 TagError>>
+Parser::collect_block() {
+    std::wstring buf;
+
+    while (this->can_advance()) {
+        const wchar_t cur = this->quick_peek();
+
+        if ('\t' == cur) {
+            this->advance();
+            continue;
+        }
+
+        const wchar_t next = this->quick_peek(1);
+
+        if (cur == next && '#' == cur) {
+            buf.push_back(cur);
+            this->advance(2);
+            continue;
+        }
+
+        if ('\n' == cur || '\r' == cur) {
+            this->advance();
+            this->advance_line();
+            continue;
+        }
+
+        if ('#' != cur) {
+            buf.push_back(cur);
+            this->advance();
+            continue;
+        }
+
+        // #<tag>
+        if (!Parser::trim(buf).empty()) {
+            return std::make_pair(
+                ParserAction::AddChild,
+                std::make_shared<Node>(std::move(Node::text(buf))));
+        }
+
+        const std::variant<std::shared_ptr<Tag>, SyntaxError> tag_res =
+            this->collect_tag();
+
+        if (std::holds_alternative<SyntaxError>(tag_res)) {
+            return std::get<SyntaxError>(tag_res);
+        }
+
+        const auto tag = std::get<std::shared_ptr<Tag>>(tag_res);
+        const std::variant<std::pair<ParserAction, std::shared_ptr<Node>>,
+                           TagError>
+            node_res = this->tag_to_node(tag);
+
+        if (std::holds_alternative<TagError>(node_res)) {
+            return std::get<TagError>(node_res);
+        }
+
+        return std::get<std::pair<ParserAction, std::shared_ptr<Node>>>(
+            node_res);
+    }
+
+    if (!Parser::trim(buf).empty()) {
+        return std::make_pair(
+            ParserAction::AddChild,
+            std::make_shared<Node>(std::move(Node::text(buf))));
+    }
+
+    return std::nullopt;
+}
+
+} // namespace louvre
